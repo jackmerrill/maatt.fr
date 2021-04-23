e7c83757b84932c94c5abfa3dbd648781d4ac2cf
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-04-23 06:55:20 -0700

    fix()

--- components/logos/logo.tsx
+++ components/logos/logo.tsx
@@ -1,4 +1,4 @@
-export default function logo () {
+export default function Logo () {
   return (
         <svg width="128" height="128" viewBox="0 0 500 500" fill="none">
             <path d="M245.344 188.5C247.269 185.167 252.08 185.167 254.004 188.5L325.019 311.5C326.943 314.833 324.537 319 320.688 319H178.66C174.811 319 172.406 314.833 174.33 311.5L245.344 188.5Z" className="stroke-current dark:text-white transition-colors group-hover:text-lightPurple" stroke="black" strokeWidth="15"/>
