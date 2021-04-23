c0023c9b7ee38c6d1472bb98705a1dfcb678101a
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-04-23 07:32:10 -0700

    chore: preloaded most compatible version of Inter

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,6 +19,11 @@ export default class _document extends Document {
           <link rel="stylesheet"
                 href="https://cdn.doamatto.xyz/inter.min.css"
                 type="text/css" />
+          <link rel="preload"
+                href="https://rsms.me/inter/font-files/Inter-roman.var.woff2?3.13"
+                as="font"
+                type="font/woff2"
+                crossOrigin="true" />
           <meta name="ocs-site-verification" content="93595f861998e6fd066083de16af45e1"></meta>
           <meta name="wot-verification" content="5140a65b9981fa17e2f8"></meta>
 
