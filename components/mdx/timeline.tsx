040cbc54d88eecad06e87aba6f508a8e545a80a5
doamatto (Ronchetto, Matthew) <hello@doamatto.xyz>
2021-04-23 13:39:40 -0700

    chore: convert styles to Tailwind (#145)

--- components/mdx/timeline.tsx
+++ components/mdx/timeline.tsx
@@ -16,21 +16,21 @@ export function parse(date:string) {
 
 export function Item ({components, ...props}) {
   return (
-    <section className={props.status}>
-      {props.title && ( <h3>{props.title}</h3> )}
+    <section className={props.status + " border-l-4 pl-2"}>
+      {props.title && ( <h3 className="font-bold text-xl">{props.title}</h3> )}
       {props?.children}
       {props.date && (
-        <h4 className="minor">{parse(props.date)}</h4>
+        <h4 className="text-sm m-0 font-bold opacity-70">{parse(props.date)}</h4>
       )}
       {props.dateRange && (
-        <h4 className="minor">{parse(props.dateRange[0])} to {props.dateRange[1] === "now" ? (
+        <h4 className="text-sm m-0 font-bold opacity-70">{parse(props.dateRange[0])} to {props.dateRange[1] === "now" ? (
           parse('now')
         ) : (
           parse(props.dateRange[1])
         ) }</h4>
       )}
       {!props.date && !props.dateRange && (
-        <h4 className="minor">Unknown time period</h4>
+        <h4 className="text-sm m-0 font-bold opacity-70">Unknown time period</h4>
       )}
     </section>
   )
