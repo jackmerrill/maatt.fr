27ce1aa677fcc485d7322a3c6e15ba99fd17c32e
Matt Ronchetto <hello@doamatto.xyz>
2021-04-27 10:33:34 -0700

    chore: added button to tailwind and as redis

--- pages/sitemap.tsx
+++ pages/sitemap.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { NextSeo } from 'next-seo'
 
 import dynamic from 'next/dynamic'
+import Button from '@/components/button'
 const Header = dynamic(() => import('@/components/header'))
 
 export default function Sitemap () {
@@ -14,12 +15,10 @@ export default function Sitemap () {
     <div id="content">
       <Header title="Sitemap" />
         <p>So far, there is only an XML version of the sitemap.An HTML version is coming soon™.</p>
-        <a
-          href="/sitemap.xml"
+        <Button
+          url="/sitemap.xml"
           title="Go to XML sitemap"
-          className="button">
-            Go to XML sitemap
-        </a>
+        />
     </div>
     </>
   )
