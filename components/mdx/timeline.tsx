a743a9eb8bf931c1dea8b470c69166027c237983
Ronchetto, Matthew <hello@doamatto.xyz>
2021-04-30 07:04:32 -0700

    chore: preparations for moving class

--- components/mdx/timeline.tsx
+++ components/mdx/timeline.tsx
@@ -17,20 +17,20 @@ export function parse(date:string) {
 export function Item ({components, ...props}) {
   return (
     <section className={props.status + " border-l-4 pl-2"}>
-      {props.title && ( <h3 className="font-bold text-xl">{props.title}</h3> )}
+      {props.title && ( <h3 className="font-bold text-xl my-3">{props.title}</h3> )}
       {props?.children}
       {props.date && (
-        <h4 className="text-sm m-0 font-bold opacity-70">{parse(props.date)}</h4>
+        <h4 className="text-sm m-0 font-bold opacity-70 my-3">{parse(props.date)}</h4>
       )}
       {props.dateRange && (
-        <h4 className="text-sm m-0 font-bold opacity-70">{parse(props.dateRange[0])} to {props.dateRange[1] === "now" ? (
+        <h4 className="text-sm m-0 font-bold opacity-70 my-3">{parse(props.dateRange[0])} to {props.dateRange[1] === "now" ? (
           parse('now')
         ) : (
           parse(props.dateRange[1])
         ) }</h4>
       )}
       {!props.date && !props.dateRange && (
-        <h4 className="text-sm m-0 font-bold opacity-70">Unknown time period</h4>
+        <h4 className="text-sm m-0 font-bold opacity-70 my-3">Unknown time period</h4>
       )}
     </section>
   )
