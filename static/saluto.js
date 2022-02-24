632be8d68b53e1fb59be98984f4c72d7e24be3ee
Matt Ronchetto <hello@doamatto.xyz>
2022-02-24 06:07:28 -0800

    Delete saluto.js

--- static/saluto.js
+++
@@ -1,3 +0,0 @@
-function n(){let t=["\u4F60\u597D","Hello","Bonjour","\uC5B4\uC774","\u3053\u3093\u306B\u3061\u308F","Hej"],e=t.length,o=Math.floor(Math.random()*e);return t[o]};
-
-window.onload = (() => {document.getElementsByClassName("title")[0].innerHTML=n()+", I'm Matt Ronchetto.";});
